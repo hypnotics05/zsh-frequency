0884e037ef52abf2ab69e40ef7ce847136009652
hypnotics <devhypnotics@proton.me>
2024-10-28 09:20:30 -0300

    Refactored code and filled out gen_hash_map()
    Removed code comments, design of functions is done the init stage,
    moving on to making sure everything I wrote actually works.
    
    collector() now returns false when file reaches it's end point.
    
    TODO: Add tests for collector(), and then gen_hash_map().

--- src/zsh.rs
+++ src/zsh.rs
@@ -1,33 +1,10 @@
-// Here is where the struct, hashmap and all methods used for the analysis will go.
-// there are invalid utf-8 characters in the file (japanese names of anime in mv operations) so
-// we need a proper way to handle this, because none of the binaries contain invalid chars if
-// we read in chars at a time and store them into a string, we can avoid reading in invalid
-// chars. Then just do substring analysis to do the find and place in the HashMap
+const SKIP_RANGE: u8 = 15; // How many chars to skip to get to cmd
 
-// A way to avoid non utf-8 chars is just to ingore it on err, but how?
-
-// Use std::io::BufRead to read file? It is very good at making frequent small read calls,
-// which we will be needing (every line is a read call), we could use peek() or an iterator
-// with bytes()
-
-// Creates the file buffer
-// let buffer = BufReader::with_capacity(file.metadata().unwrap().len() as usize, file);
-
-// Buffer should create an Iterator, with this we could call next(), which return a reference to
-// the value, we could collect these references in some sort of collector (Vec<>?) while the
-// ref is not ' ', then turn the collection to a string and place it into the HashMap. Then we need
-// to next til '\n' then we can skip 15 chars with advance_by()
-
-// Use BufReader::read_until(), ' ' and '\n' and ';'
-
-// Still need a way to ignore u16 when iterating over BufReader()
-// try using a match statment with Some(x) use x, with None we could skip, but because the iterator
-// returns None when there are no more bytes to iterate, we should count consecutive Nones and if
-// the None surpass a limit, we can exit.
-
-const SKIP_RANGE: u8 = 14; // How many chars to skip to get to cmd
-
-//const NONE_RATE: u8 = 255; // How many non UTF-8 chars we can meet before we give up on the file
+/*
+ * Example of the line that we need to parse
+ * : 1729482722:0;systemctl hybrid-sleep
+ * : 1729543437:0;eval $(ssh-agent)
+ */
 
 use std::{
     collections::HashMap,
@@ -35,26 +12,28 @@ use std::{
     io::{BufReader, Bytes, Read},
 };
 
-pub fn genHashMap(file: File) -> HashMap<String, usize> {
-    let mut map = HashMap::new();
+pub fn gen_hash_map(file: File) -> HashMap<String, usize> {
+    let mut map: HashMap<String, usize> = HashMap::new();
     let mut prog: Vec<u8> = Vec::new(); // here is where we collect
     let mut iter = BufReader::with_capacity(file.metadata().unwrap().len() as usize, file).bytes();
-
-    // TODO: Add collector and skip function calls here
-    iter.next(); // buffer to remove the ':' from the first case
-
-    /*
-     * Example of the line types that we need to parse
-     * : 1729482722:0;systemctl hybrid-sleep
-     * : 1729543437:0;eval $(ssh-agent)
-     */
+    let mut more: bool = true;
+    let mut bin: String;
+    while more {
+        more = collector(&mut iter, &mut prog);
+        bin = String::from_utf8(prog.clone()).unwrap();
+        let key = map.entry(bin).or_insert(1);
+        *key += 1;
+    }
     map
 }
 
-fn collector(iter: &mut Bytes<BufReader<File>>, prog: &mut Vec<u8>) {
-    for _ in [..SKIP_RANGE] {
+// keeping this as seperate function for now, incase we want to expand functionality at some point
+fn collector(iter: &mut Bytes<BufReader<File>>, prog: &mut Vec<u8>) -> bool {
+    // skips ": NUM:0;"
+    for _ in 0..=SKIP_RANGE {
         iter.next();
     }
+    // tries to build string
     loop {
         match iter.next() {
             Some(x) => match x {
@@ -65,17 +44,12 @@ fn collector(iter: &mut Bytes<BufReader<File>>, prog: &mut Vec<u8>) {
                         break;
                     }
                 }
-                _ => continue, // catching any error here because Err doesn't work? Need to
-                               // insvestigate more later.
+                _ => continue, // catching any error here because Err doesn't work?
             },
-            None => break,
+            // Might not be nescesary as if this where to occur, the file is missformatted
+            None => return false, // exit function, no more file to read
         }
     }
-}
-/*
- * Looping over iter until we meet the patern '\n:'
- */
-fn skip(iter: &mut Bytes<BufReader<File>>) {
     let mut str1: u8 = ' ' as u8;
     let mut str2: u8;
     loop {
@@ -85,15 +59,15 @@ fn skip(iter: &mut Bytes<BufReader<File>>) {
                     str2 = str1;
                     str1 = x;
                     if str1 == '\n' as u8 && str2 == ':' as u8 {
-                        return;
+                        return true;
                     }
                 }
                 _ => {
-                    str2 = str1;
+                    str2 = str1; // still don't know how to fix this
                     str1 = ' ' as u8;
                 }
             },
-            None => break,
+            None => return false,
         }
     }
 }
