8154bedecf3383ca2e56069adbe4b268fb3eff15
hypnotics <devhypnotics@proton.me>
2024-10-28 12:01:53 -0300

    Fixed gen_hash_map()
    Problem: gen_hash_map() keys where missformatted and value was off
    
    Solution:
     To fix the keys being missformated, SKIP_RANGE changed from 15 to 14,
     fixing an off by one error where the first letter was skipped.
    
     Some keys had value '\n:' added to them, by checking for '\n' before
     pushing them to prog, we can store them in a buffer var and stop it
     from being added to the vector.
    
     Val was fixed by making default new values be 0 instead of 1.

--- src/zsh.rs
+++ src/zsh.rs
@@ -1,4 +1,4 @@
-const SKIP_RANGE: u8 = 15; // How many chars to skip to get to cmd
+const SKIP_RANGE: u8 = 14; // How many chars to skip to get to cmd
 
 /*
  * Example of the line that we need to parse
@@ -6,40 +6,50 @@ const SKIP_RANGE: u8 = 15; // How many chars to skip to get to cmd
  * : 1729543437:0;eval $(ssh-agent)
  */
 
+// TODO: add support for sudo, both by reading command after sudo and counting the number of sudo
+// TODO: Add support to read inline commands
+
 use std::{
     collections::HashMap,
     fs::File,
     io::{BufReader, Bytes, Read},
 };
 
-// FIXME: hash map is missing most values, why?
 pub fn gen_hash_map(file: File) -> HashMap<String, usize> {
     let mut map: HashMap<String, usize> = HashMap::new();
     let mut prog: Vec<u8> = Vec::new(); // here is where we collect
     let mut iter = BufReader::with_capacity(file.metadata().unwrap().len() as usize, file).bytes();
     let mut more: bool = true;
     let mut bin: String;
+    iter.next();
     while more {
         more = collector(&mut iter, &mut prog);
         bin = String::from_utf8(prog.clone()).unwrap();
-        let key = map.entry(bin).or_insert(1);
+        let key = map.entry(bin).or_insert(0);
         *key += 1;
+        let _ = &mut prog.clear();
     }
     map
 }
 
-// keeping this as seperate function for now, incase we want to expand functionality at some point
 fn collector(iter: &mut Bytes<BufReader<File>>, prog: &mut Vec<u8>) -> bool {
     // skips ": NUM:0;"
     for _ in 0..SKIP_RANGE {
         iter.next();
     }
     // tries to build string
+    let mut has_buf: bool = false;
+    let mut buf: u8 = 0;
     loop {
         match iter.next() {
             Some(x) => match x {
                 Ok(y) => {
                     if y != ' ' as u8 {
+                        if y == '\n' as u8 {
+                            has_buf = true;
+                            buf = y;
+                            break;
+                        }
                         prog.push(y);
                     } else {
                         break;
@@ -54,13 +64,19 @@ fn collector(iter: &mut Bytes<BufReader<File>>, prog: &mut Vec<u8>) -> bool {
     }
     let mut str1: u8 = ' ' as u8;
     let mut str2: u8;
+    if has_buf {
+        str1 = buf;
+        has_buf = false;
+    }
     loop {
         match iter.next() {
             Some(x) => match x {
                 Ok(x) => {
                     str2 = str1;
                     str1 = x;
-                    if str1 == '\n' as u8 && str2 == ':' as u8 {
+                    // HACK: debug statment to determine what the stream contains
+                    // println!("Reading: 1 - {} && 2 - {}", str1 as char, str2 as char);
+                    if str1 == ':' as u8 && str2 == '\n' as u8 {
                         return true;
                     }
                 }
@@ -116,17 +132,31 @@ mod test {
     fn test_collector_build_single() {
         let mut prog: Vec<u8> = Vec::new();
         let mut iter = iterator("test/collector-build-single.log");
+        iter.next();
         let _ = collector(&mut iter, &mut prog);
         assert_eq!(prog, vec_u8_from_str("systemctl"));
     }
 
-    // Failing test, only "systemcl" is being read, and only twice.
     #[test]
     fn test_collector_build_map() {
         assert_eq!(
             gen_hash_map(open_file("test/collector-build-map.log")),
             build_default_map()
         );
+    }
+
+    #[test]
+    fn test_collector_multi() {
+        let mut first: Vec<u8> = Vec::new();
+        let mut second: Vec<u8> = Vec::new();
+        let mut iter = iterator("test/collector-2-strings.log");
+        iter.next();
+        let _ = collector(&mut iter, &mut first);
+        let _ = collector(&mut iter, &mut second);
+
+        assert_eq!(first, vec_u8_from_str("systemctl"));
+        // grep is not even being read here, maybe stop patern does not work?
+        assert_eq!(second, vec_u8_from_str("grep"));
     }
 
     // tests if skip section can infact skip past utf-16 chars
